17c0f3c78a64e891662fdd0612b910f4a991bbb6
GustaJLemos <gustavolemosmendes@gmail.com>
2023-06-21 22:29:49 -0300

    Adicionando AppProvider e USerProvider do realm

--- App.tsx
+++ App.tsx
@@ -5,6 +5,10 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { Loading } from './src/components/Loading';
 import { StatusBar } from 'react-native';
 
+import { AppProvider, UserProvider } from '@realm/react'
+import { REAL_APP_ID } from '@env';
+import { Home } from './src/screens/Home';
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
@@ -15,14 +19,23 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle='light-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REAL_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor='transparent'
+          translucent
+        />
+        {/* UserProvider vai ser responsável pela parte de autenticação mesmo */}
+        {/* O fallback basicamente fala se não tiver nenhum usuário autenticado, então a gente chama esse método (componente SignIn) para autenticar */}
+        <UserProvider fallback={<SignIn />}>
+          {/* Se o usuário já estiver autenticado ele renderiza esse cara dentro do UserProvider */}
+          <Home />
+          {/* Ou seja, oq vai rolar aq? se o usuário não estiver autenticado ele vai ser levado pra nossa fallback, e vai ser levado pra nossa tela de login, e se 
+          o usuário já estiver autenticado vai ser levado para o nossa home */}
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
 
