a1df526cdffe290ac387a7ea110e06d9e4f88ca2
Gustavo José Lemos Mendes <gustavo.mendes@w-security.intranet>
2023-06-22 17:56:00 -0300

    Adicionando Provider e routes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 import { SignIn } from './src/screens/SignIn';
 import theme from './src/theme';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from './src/components/Loading';
 import { StatusBar } from 'react-native';
-
-import { AppProvider, UserProvider } from '@realm/react'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AppProvider, UserProvider } from '@realm/react';
 import { REAL_APP_ID } from '@env';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
+import { RealmProvider } from './src/libs/realm';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,21 +20,28 @@ export default function App() {
   }
 
   return (
+    // compartilhar a integração com o atlas com nosso app
     <AppProvider id={REAL_APP_ID}>
       <ThemeProvider theme={theme}>
-        <StatusBar
-          barStyle='light-content'
-          backgroundColor='transparent'
-          translucent
-        />
-        {/* UserProvider vai ser responsável pela parte de autenticação mesmo */}
-        {/* O fallback basicamente fala se não tiver nenhum usuário autenticado, então a gente chama esse método (componente SignIn) para autenticar */}
-        <UserProvider fallback={<SignIn />}>
-          {/* Se o usuário já estiver autenticado ele renderiza esse cara dentro do UserProvider */}
-          <Home />
-          {/* Ou seja, oq vai rolar aq? se o usuário não estiver autenticado ele vai ser levado pra nossa fallback, e vai ser levado pra nossa tela de login, e se 
+        <SafeAreaProvider style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_800 }}>
+          <StatusBar
+            barStyle='light-content'
+            backgroundColor='transparent'
+            translucent
+          />
+          {/* UserProvider vai ser responsável pela parte de autenticação mesmo */}
+          {/* O fallback basicamente fala se não tiver nenhum usuário autenticado, então a gente chama esse método (componente SignIn) para autenticar */}
+          <UserProvider fallback={<SignIn />}>
+            {/* Se o usuário já estiver autenticado ele renderiza esse cara dentro do UserProvider */}
+
+            {/* importante que o realm provider esteja dentro do useProvider, pq dai ele já compartilha o nosso user logado dentro do realm */}
+            <RealmProvider>
+              <Routes />
+            </RealmProvider>
+            {/* Ou seja, oq vai rolar aq? se o usuário não estiver autenticado ele vai ser levado pra nossa fallback, e vai ser levado pra nossa tela de login, e se 
           o usuário já estiver autenticado vai ser levado para o nossa home */}
-        </UserProvider>
+          </UserProvider>
+        </SafeAreaProvider>
       </ThemeProvider>
     </AppProvider>
   );
