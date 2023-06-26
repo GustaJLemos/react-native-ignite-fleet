f4fd7e4b4ec0b1fe481e8450fdd6bdee2fb10bf2
Gustavo José Lemos Mendes <gustavo.mendes@w-security.intranet>
2023-06-26 18:42:35 -0300

    Adicionando o componente topMessage

--- App.tsx
+++ App.tsx
@@ -11,10 +11,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppProvider, UserProvider } from '@realm/react';
 import { REAL_APP_ID } from '@env';
 import { Routes } from './src/routes';
-import { RealmProvider } from './src/libs/realm';
+import { RealmProvider, syncConfig } from './src/libs/realm';
+import { TopMessage } from './src/components/TopMessage';
+import { WifiSlash } from 'phosphor-react-native';
+import { useNetInfo } from '@react-native-community/netinfo';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const netInfo = useNetInfo();
 
   if (!fontsLoaded) {
     return (
@@ -32,13 +36,21 @@ export default function App() {
             backgroundColor='transparent'
             translucent
           />
+          {
+            !netInfo.isConnected &&
+            <TopMessage
+              title='Você está off-line'
+              icon={WifiSlash}
+            />
+          }
           {/* UserProvider vai ser responsável pela parte de autenticação mesmo */}
           {/* O fallback basicamente fala se não tiver nenhum usuário autenticado, então a gente chama esse método (componente SignIn) para autenticar */}
           <UserProvider fallback={<SignIn />}>
             {/* Se o usuário já estiver autenticado ele renderiza esse cara dentro do UserProvider */}
 
             {/* importante que o realm provider esteja dentro do useProvider, pq dai ele já compartilha o nosso user logado dentro do realm */}
-            <RealmProvider>
+            {/* Enquanto está no processo de abrir banco de dados, e etc... a gente deixa o Loading */}
+            <RealmProvider sync={syncConfig} fallback={Loading}>
               <Routes />
             </RealmProvider>
             {/* Ou seja, oq vai rolar aq? se o usuário não estiver autenticado ele vai ser levado pra nossa fallback, e vai ser levado pra nossa tela de login, e se 
