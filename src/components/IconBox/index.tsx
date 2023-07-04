b449af33062a24544a5b1e707c35c55e4076841c
GustaJLemos <gustavolemosmendes@gmail.com>
2023-07-03 22:23:09 -0300

    Criando os componentes

---
+++ src/components/IconBox/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, SizeProps } from './styles';
+import { IconProps } from 'phosphor-react-native';
+import { useTheme } from 'styled-components';
+
+export type IconBoxProps = (props: IconProps) => JSX.Element;
+
+type Props = {
+  size?: SizeProps;
+  icon: IconBoxProps;
+}
+
+export function IconBox({ size = 'NORMAL', icon: Icon }: Props) {
+  const theme = useTheme();
+
+  const iconSize = size === 'NORMAL' ? 24 : 16
+
+  return (
+    <Container size={size}>
+      <Icon
+        size={iconSize}
+        color={theme?.COLORS.BRAND_LIGHT}
+      />
+    </Container>
+  );
+}
